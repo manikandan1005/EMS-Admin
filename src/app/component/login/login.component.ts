e48f2f43cd5249547358582e1114cc94b8e2cda5
Manikandan S <ceitmanikandan24@gmail.com>
2026-02-07 22:04:30 +0530

    update the login component

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {  Router } from '@angular/router';
 interface data{userName:string,pass1:number|null,pass2:number|null}
 @Component({
   selector: 'app-login',
@@ -8,9 +9,12 @@ interface data{userName:string,pass1:number|null,pass2:number|null}
 })
 export class LoginComponent {
   login:data={userName:'',pass1:null,pass2:null};
+  router=inject(Router);
   Login=()=>{
     if(this.login.pass1===this.login.pass2 && this.login.pass1 !=null){
       console.log("pass word match");
+      this.router.navigateByUrl('dashboard');
+
     }
     else alert("Passwords do not match");
 
