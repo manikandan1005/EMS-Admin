bc1074c8fddcdb0704e250f585da5b4cf4063f7c
Manikandan S <ceitmanikandan24@gmail.com>
2026-02-07 22:02:19 +0530

    update the nav

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
-
+  title = "StaHat";
+  list = [
+    { rout: 'dashboard', name: "Dashboard" },
+    { rout: "employee", name: "Employees" },
+    { name: "jobs", rout: "job" },
+    { name: "Tasks", rout: "task" },
+    { name: "Leave management", rout: "leav" }
+  ]
 }
