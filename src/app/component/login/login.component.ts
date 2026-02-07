4d9c61c6b37eba98c0bd2a50f43329a1b242092e
Manikandan S <ceitmanikandan24@gmail.com>
2026-02-07 20:51:27 +0530

    update the login component

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+interface data{userName:string,pass1:number|null,pass2:number|null}
 @Component({
   selector: 'app-login',
   standalone: false,
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  login:data={userName:'',pass1:null,pass2:null};
+  Login=()=>{
+    if(this.login.pass1===this.login.pass2 && this.login.pass1 !=null){
+      console.log("pass word match");
+    }
+    else alert("Passwords do not match");
 
+  }
 }
