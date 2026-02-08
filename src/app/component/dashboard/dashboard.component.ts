b39b1c63916e282a2316e06d9fce97f754e35964
Manikandan S <ceitmanikandan24@gmail.com>
2026-02-08 17:54:16 +0530

    design dashboard

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+icons = [
+  { class: 'fa-regular fa-bell text-red-500 w-6 h-6' },
+  { class: 'fa-solid fa-gear text-blue-500 w-6 h-6' },
+  { class: 'fa-regular fa-envelope text-green-500 w-6 h-6' }
+];
 
+
+  Dashboard=[
+    {
+      icon:'',
+      count:2,
+      name:'message'
+    },
+    {
+      icon:'',
+      count:2,
+      name:'message'
+    },{
+      icon:'',
+      count:2,
+      name:'message'
+    },{
+      icon:'',
+      count:2,
+      name:'message'
+    }
+  ]
 }
