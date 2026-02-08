fcf3a45958eb7259a2e77d690facd12468ce08fb
Manikandan S <ceitmanikandan24@gmail.com>
2026-02-08 17:57:11 +0530

    design the employee and get data from backend

--- src/app/component/employee/employee.component.ts
+++ src/app/component/employee/employee.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { DataServicesService } from '../../service/data-services.service';
+
 
 @Component({
   selector: 'app-employee',
@@ -6,6 +9,41 @@ import { Component } from '@angular/core';
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
 
+export class EmployeeComponent implements OnInit {
+
+  // http=inject(HttpClient);
+  dataService=inject(DataServicesService)
+  tableHeader: string[] = [
+    'Employee ID',
+    'Name',
+    'Role',
+    
+    'Gender',
+    'Email ID',
+    'Address',
+    'Work Mode','Action'
+  ];
+  employeeList:any[]=[]
+  ngOnInit(): void {
+  //     fetch("http://localhost:3000/api/data").then(res=>res.json())
+  //     .then(data=>console.log(data))
+  //     .catch(err=>console.log(err))
+  // }
+    this.getData();
+    
+  }
+  getData(){
+    this.dataService.getEmployee().subscribe({
+      next:data=>this.employeeList=data,
+      error:err=>alert(err.message)
+    })
+  }
+
+
+  remove=(id:string)=>{
+    const userId=id;
+    console.log(`remove ${userId}`)
+  }
+  // console.log(this.employees)
 }
